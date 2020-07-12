62ddddac9751ce22dd35711322e2057c6ac9261f
saurabh <saurabhbharti9@gmail.com>
2020-07-12 09:39:40 +0530

    added login form-control

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 export default function Login() {
+  const [username, setusername] = useState("");
+  const [password, setPwd] = useState("");
+  const [room, setRoom] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log(username, password, room);
+  };
   return (
     <div className="join-container">
-      <header class="join-header">
+      <header className="join-header">
         <h1>
-          <i class="fas fa-smile"></i> let's talk
+          <i className="fas fa-smile"></i> let's talk
         </h1>
       </header>
+      <main className="join-main">
+        <form onSubmit={handleLogin}>
+          <div className="form-control">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              value={username}
+              placeholder="Enter username..."
+              required
+              onChange={(event) => setusername(event.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              name="password"
+              value={password}
+              placeholder="Enter password..."
+              required
+              onChange={(event) => setPwd(event.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="room">Room</label>
+            <select
+              name="room"
+              value={room}
+              onChange={(event) => setRoom(event.target.value)}
+            >
+              <option value="JavaScript">JavaScript</option>
+              <option value="Python">Python</option>
+              <option value="PHP">PHP</option>
+              <option value="C#">C#</option>
+              <option value="Ruby">Ruby</option>
+              <option value="Java">Java</option>
+            </select>
+          </div>
+          <button type="submit" className="btn">
+            Join Chat
+          </button>
+        </form>
+      </main>
     </div>
   );
 }
