f6e74f9bab0f15571d1179f2c3cdf81af1629209
saurabh <saurabhbharti9@gmail.com>
2020-07-13 21:45:53 +0530

    prod-build

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const bodyparser = require("body-parser");
 const path = require("path");
+const cors = require("cors");
 //initiliaze express app
 const app = express();
 
 //applymiddlewares
 app.use(bodyparser.json());
 dotenv.config();
+app.use(cors(), bodyparser.urlencoded({ extended: true }), bodyparser.json());
 let port = process.env.PORT || process.env.API_PORT;
 app.listen(port, () => console.log("API Server running on", port));
 
