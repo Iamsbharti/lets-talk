c692cffa8aebfd7c3f5c27e0253a9417127fe383
saurabh <saurabhbharti9@gmail.com>
2020-07-13 21:24:47 +0530

    prod-build

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyparser.json());
 dotenv.config();
 let port = process.env.PORT || process.env.API_PORT;
-app.listen(port, () => console.log("Server running on", port));
+app.listen(port, () => console.log("API Server running on", port));
 
 app.get("/test", (req, res) => {
   console.log("test route");
@@ -17,7 +17,7 @@ app.get("/test", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  console.log("prod");
+  console.log("prod-env");
   app.use(express.static(path.resolve(__dirname, "../../build")));
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve("index.html"));
