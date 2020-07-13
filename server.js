cc03175abb56064e4c6a296c02f5f1790cf97f0c
saurabh <saurabhbharti9@gmail.com>
2020-07-13 21:18:38 +0530

    added server and prepared for deploy

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyparser = require("body-parser");
-
+const path = require("path");
 //initiliaze express app
 const app = express();
 
 //applymiddlewares
 app.use(bodyparser.json());
 dotenv.config();
+let port = process.env.PORT || process.env.API_PORT;
+app.listen(port, () => console.log("Server running on", port));
 
-app.listen(4001, () => console.log("Server running on 3001"));
+app.get("/test", (req, res) => {
+  console.log("test route");
+  res.send("test works");
+});
+
+if (process.env.NODE_ENV === "production") {
+  console.log("prod");
+  app.use(express.static(path.resolve(__dirname, "../../build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve("index.html"));
+  });
+}
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,15 @@
 import React from "react";
-
+const url =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:4300/test";
 export default function Register() {
-  return <div>Register</div>;
+  const api = () => {
+    console.log("api call");
+    fetch(url).then((data) => console.log(data));
+  };
+  api();
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  );
 }
