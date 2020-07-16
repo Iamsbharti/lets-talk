8594bbccc4975ca8e621192a44403855b4a8d29e
saurabh <saurabhbharti9@gmail.com>
2020-07-16 10:10:37 +0530

    implemented login

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,26 +1,68 @@
 const User = require("../models/User");
-const { response, hashedPassword, compareHashedPwd } = require("../lib");
+const {
+  response,
+  hashedPassword,
+  compareHashedPwd,
+  generateToken,
+} = require("../lib");
 exports.loginControl = async (req, res) => {
   console.log("login control control");
   const { email, password } = req.body;
-  //check user existence
-  let userExists = await User.findOne({ email: email });
-
-  /**If not found return  */
-  if (!userExists)
-    return res
-      .status(400)
-      .send(response(true, 400, "User Not Found ,Register", email));
-
-  /**credential match if user found */
-  let validCred = await compareHashedPwd(userExists.password, password);
-  if (!validCred)
-    return res.status(400).json(true, 400, "Wrong Password", null);
-
+  //find user
+  const emailExistence = async () => {
+    console.log("email existence call");
+    let user = await User.findOne({ email: email });
+    //console.log(user.password);
+    return !user
+      ? Promise.reject(response(true, 404, "user doesn't exists", email))
+      : Promise.resolve(user);
+  };
+  //credmatch
+  const credentialMatch = async (user) => {
+    let credMatch = await compareHashedPwd(user.password, password);
+    if (credMatch) {
+      /** delete additional properties of user*/
+      let userDetails = user.toObject();
+      delete userDetails.password;
+      delete userDetails.__v;
+      delete userDetails._id;
+      return Promise.resolve(userDetails);
+    } else {
+      return Promise.reject(
+        response(true, 400, "Password Doesn't match", null)
+      );
+    }
+  };
   //generate token
-  if (validCred) {
-  }
+  const getToken = async (userDetails) => {
+    let getTokenRes;
+    await generateToken(userDetails, (error, tokenDetails) => {
+      if (error !== null) {
+        getTokenRes = Promise.reject(
+          response(true, 500, "Token Generation Error", error)
+        );
+      } else {
+        tokenDetails.userDeatils = userDetails;
+        getTokenRes = Promise.resolve(tokenDetails);
+      }
+    });
+    return getTokenRes;
+  };
   //save token
+  const saveToken = async (tokenDetails) => {
+    console.log("save token", tokenDetails);
+  };
+  //login func start
+  emailExistence()
+    .then(credentialMatch)
+    .then(getToken)
+    .then(saveToken)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 exports.test = async (req, res) => {
   console.log("test route");
--- server/lib/index.js
+++ server/lib/index.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const shortid = require("shortid");
 exports.response = (error, status, message, data) => {
   let res = {
     error: error,
@@ -17,4 +19,24 @@ exports.hashedPassword = async (pwd) => {
 exports.compareHashedPwd = async (hashedPwd, pwd) => {
   console.log("compare passwords");
   return await bcrypt.compare(pwd, hashedPwd);
+};
+exports.generateToken = async (data, cb) => {
+  console.log("generate token");
+  try {
+    let tokenClaims = {
+      jwtid: shortid.generate(),
+      iat: Date.now(),
+      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 48,
+      sub: "authToken",
+      iss: "react-chat-app",
+      data: data,
+    };
+    let tokenDetails = {
+      authToken: jwt.sign(tokenClaims, process.env.TOKEN_SECRET),
+    };
+    cb(null, tokenDetails);
+  } catch (error) {
+    console.log("Token gen error", error);
+    cb(error, null);
+  }
 };
