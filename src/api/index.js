9f8e169424be635cb75116f4ff93de4dec56decc
saurabh <saurabhbharti9@gmail.com>
2020-07-18 12:54:18 +0530

    added register component and api call

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 
 const url =
-  process.env.NODE_ENV === "production"
-    ? ""
-    : "http://localhost:4300/api/chat/register";
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:4300/api/chat";
 export async function login(email, password) {}
 export async function register(firstName, lastName, email, password) {
   console.log("call register");
   //call register route
   try {
-    let { data, status } = await axios.post(url, {
+    let { data, status } = await axios.post(url + "/register", {
       firstName: firstName,
       lastName: lastName,
       email: email,
