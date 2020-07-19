fcd65c19bc488612f8d03b2424fe9384c8e28698
saurabh <saurabhbharti9@gmail.com>
2020-07-19 10:32:59 +0530

    added chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,44 @@
 import React from "react";
 import "../App.css";
-
-function Chat() {
-  return <div>Chat Box</div>;
+import { connect } from "react-redux";
+function Chat({ roomname }) {
+  return (
+    <div className="chat-container">
+      <header className="chat-header">
+        <h1>
+          <i className="fas fa-comments"></i>Let's Talk
+        </h1>
+      </header>
+      <main className="chat-main">
+        <div className="chat-sidebar">
+          <h3>
+            <i className="far fa-comment-alt">Room Name</i>
+          </h3>
+          <h2>{roomname}</h2>
+          <h3>
+            <i className="fas fa-users"></i>Users
+          </h3>
+          <ul></ul>
+        </div>
+        <div className="chat-messages"></div>
+      </main>
+      <div className="chat-form-container">
+        <form id="chat-form">
+          <input
+            id="msg"
+            type="text"
+            placeholder="Enter Message"
+            required
+            autoComplete="off"
+          />
+          <button className="btn">
+            <i className="fas fa-paper-plane"></i>Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
-export default Chat;
+const mapStateToProps = () => {};
+const mapActionToProps = {};
+export default connect(mapStateToProps, mapActionToProps)(Chat);
